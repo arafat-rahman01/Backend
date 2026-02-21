059ccc4cd186c813c186ee9565a8610f167d7985
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-21 23:59:57 +0600

    work

--- Node_js1/script.js
+++ Node_js1/script.js
@@ -2,3 +2,9 @@ let a=5;
 for(let i=0;i<5;i++){
     console.log("Updation",i);
 }
+console.log(process.argv);
+
+//[1]  powerShell PS C:\Users\access 213\Desktop\Web dev class\Backend>
+//[2]  cd node_js1
+//[3]  dir
+//[4]  node script.js hello world
