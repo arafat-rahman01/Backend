36fe6552582c17fc28d23342801edd25748eabcf
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-22 02:13:47 +0600

    fun & object

---
+++ Node_js1/math.js
@@ -0,0 +1,21 @@
+//[1]
+// function add(a, b) {
+//     return a + b;
+// }
+
+// module.exports = add;
+
+
+//[2]
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+module.exports = {
+    add,
+    sub
+};
--- Node_js1/script.js
+++ Node_js1/script.js
@@ -1,16 +1,22 @@
-let a=5;
-for(let i=0;i<5;i++){
-    console.log("Updation",i);
-}
-console.log(process.argv);
+// let a=5;
+// for(let i=0;i<5;i++){
+//     console.log("Updation",i);
+// }
+// console.log(process.argv);
 
 //[1]  powerShell PS C:\Users\access 213\Desktop\Web dev class\Backend>
 //[2]  cd node_js1
 //[3]  dir
 //[4]  node script.js hello world
 
-let args=process.argv;
+// let args=process.argv;
 
-for(let i=2;i<args.length;i++){
-    console.log("Hello to ", args[i]);
-}
+// for(let i=2;i<args.length;i++){
+//     console.log("Hello to ", args[i]);
+// }
+
+
+
+const math = require("./math");
+
+console.log(math.add(5, 3));
