757c940b6e824c74c0915a890a6fad75828a51ca
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-22 23:25:02 +0600

    npm init -y /express...

--- figlet/index.js
+++ figlet/index.js
@@ -1,6 +1,6 @@
 const figlet = require("figlet");
 
-figlet("ARAFAT &  SAFA", function (err, data) {
+figlet("ARAFAT", function (err, data) {
     if (err) {
         console.log("Error happened");
         return;
