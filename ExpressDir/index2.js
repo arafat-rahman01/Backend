97003f355cea96e48b9d9ce2fb72d641e6d0ad2a
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-25 19:49:42 +0600

    route with parameters

--- ExpressDir/index2.js
+++ ExpressDir/index2.js
@@ -9,9 +9,23 @@ app.listen(port,()=>{
 
 
 //Query
+//http://localhost:8080/search?name=Arafat&age=23
+
 app.get("/search",(req,res)=>{
     console.log(req.query);
     console.log(req.query.name);
 
     res.send(`Hello ${req.query.name}, Age: ${req.query.age}`);
+});
+
+//Parameters
+//http://localhost:8080/search/Arafat
+
+app.get("/search/:name", (req, res) => {
+    res.send(`Hello ${req.params.name}`);
+});
+
+//Two parameters
+app.get("/search/:name/:age", (req, res) => {
+    res.send(`Hello ${req.params.name}, Age: ${req.params.age}`);
 });
