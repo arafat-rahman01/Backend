50f835907e5878539772d4bbf0346d243902491b
arafat-rahman01 <aabrarbin851@gmail.com>
2026-02-26 23:57:37 +0600

    static

---
+++ EJSDIR/index4.js
@@ -0,0 +1,23 @@
+const express=require("express");
+const app=express();
+const path=require("path");
+
+const port=3000;
+
+app.set("view engine","ejs");
+app.set("views",path.join(__dirname,"/views"));
+
+app.listen(port,()=>{
+    console.log(`App is running port ${port}`);
+});
+
+app.get("/", (req, res) => {
+    res.send("Home Page");
+});
+
+//static
+app.get("/students", (req, res) => {
+    let students = ["Arafat", "Rahim", "Karim", "Sakib"];
+    res.render("students", { students });
+});
+
