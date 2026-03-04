5e539a447a2b6b64146dbbff04cb5c276437bb54
arafat-rahman01 <aabrarbin851@gmail.com>
2026-03-04 21:47:11 +0600

    Home Route

--- SQLCLASS/index.js
+++ SQLCLASS/index.js
@@ -1,5 +1,8 @@
 const { faker } = require('@faker-js/faker');
 const mysql = require("mysql2");
+const express=require("express");
+
+const app=express();
 
 const connection=mysql.createConnection({
   host: 'localhost',
@@ -14,23 +17,34 @@ let getRandomUser=()=>{
     faker.internet.username(),
     faker.internet.email(),
     faker.internet.password(),
-  ];
+  ]; 
 }
 
-let q="INSERT INTO user(id,username,email,password) VALUES ?";
+// let q="INSERT INTO user(id,username,email,password) VALUES ?";
 
-let data=[];
-for(let i=1;i<=100;i++){
-  data.push(getRandomUser()); 
-}
+// let data=[];
+// for(let i=1;i<=100;i++){
+//   data.push(getRandomUser()); 
+// }
 
-try{
-    connection.query(q,[data],(err,result)=>{
-    if(err) throw err;
-    console.log(result);
-  });
-}catch(err){
-  console.log(err);
-}
 
-connection.end();
+app.get("/",(req,res)=>{
+  let q= `SELECT count(*) FROM user`;
+    try{
+      connection.query(q,(err,result)=>{
+      if(err) throw err;
+      console.log(result);
+      res.send(result);
+    });
+  }catch(err){
+    console.log(err);
+    res.send("Some error in database");
+  }
+});
+
+
+app.listen("8080",()=>{
+  console.log( `Serever is listening 8080`);
+});
+
+//connection.end();
