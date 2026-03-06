a625072958196b2dc7c6b5a0379057851301b923
arafat-rahman01 <aabrarbin851@gmail.com>
2026-03-07 02:01:27 +0600

    insertMany

---
+++ mongoDB/Aggregate.js
@@ -0,0 +1,23 @@
+db.posts.insertMany(
+    {
+    _id: 1,
+    category: "News",
+    likes: 10,
+    views: 100
+    },
+
+    {
+    _id: 2,
+    category: "News",
+    likes: 5,
+    views: 50
+    },
+
+    {
+    _id: 3,
+    category: "Event",
+    likes: 20,
+    views: 200
+    }
+)
+
