727783f7179681f44f4a845bb056f9c32b12bf74
arafat-rahman01 <aabrarbin851@gmail.com>
2026-03-06 23:50:35 +0600

    upsert=update+insert

--- mongoDB/mongo.js
+++ mongoDB/mongo.js
@@ -95,3 +95,19 @@ posts> db.posts.find()
   }
 ]
 
+//Post Update
+posts> db.posts.updateOne( 
+   { title: "Post Title 5" }, 
+   {
+     $set: 
+       {
+         title: "Post Title 5",
+         body: "Body of post.",
+         category: "Event",
+         likes: 5,
+         tags: ["news", "events"],
+         date: Date()
+       }
+   }, 
+   { upsert: true }
+ )
