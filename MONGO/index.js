bbf013aa8a93dfa8669aa109d962d1ca4e88f1b5
arafat-rahman01 <aabrarbin851@gmail.com>
2026-03-08 16:28:23 +0600

    multiple insert & find method

--- MONGO/index.js
+++ MONGO/index.js
@@ -16,4 +16,40 @@ const userSchema=new mongoose.Schema({
     age: Number
 });
 
+//Create Model
 const User=mongoose.model("User",userSchema);
+
+User.find({age :{$gt:50}})
+.then((res)=>{
+    console.log(res);
+    console.log(res[0].name); //Object+ Specific name
+})
+.catch((err)=>{
+    console.log(err);
+})
+
+//Insert One
+// const user1=new User({
+//     name: "Arafat",
+//     email: "arafat@yahoo.in",
+//     age: 48
+// });
+
+// user1
+//     .save()
+//     .then((res)=>{
+//         console.log(res);
+//     })
+//     .catch((err)=>{
+//         console.log(err);
+//     })
+
+//Insert One
+
+// User.insertMany([
+//     {name: "Tony", email: "tony@gmail.com", age: 50},
+//     {name: "Arafat", email: "Arafat@gmail.com", age: 25},
+//     {name: "Bruce", email: "bruce@gmail.com", age: 67}
+// ]).then((res)=>{
+//     console.log(res);
+// })
