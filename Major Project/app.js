73e3bffbebb5cb74f44fb0820881ca64b06e9c44
arafat-rahman01 <aabrarbin851@gmail.com>
2026-03-14 01:07:43 +0600

    mongoose connection

--- Major Project/app.js
+++ Major Project/app.js
@@ -2,6 +2,20 @@ const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
 
+const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
+
+main()
+    .then(()=>{
+        console.log("connect to DB");
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
 app.get("/",(req,res)=>{
     res.send("Hi, i'm root");
 });
