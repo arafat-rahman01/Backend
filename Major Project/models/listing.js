6a06b414b3141e8489e1aa26c1674dfef0c42461
arafat-rahman01 <aabrarbin851@gmail.com>
2026-03-15 01:54:16 +0600

    included data

--- Major Project/models/listing.js
+++ Major Project/models/listing.js
@@ -1,22 +1,22 @@
-const mongoose=require("mongoose");
-const Schema=mongoose.Schema;
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const listingSchema=new Schema({
-    title:{
-        type:String,
-        required:true,
-    },
-    description: String,
-    image:{
-        type: String,
-        default:
-            "https://unsplash.com/photos/blue-red-and-white-striped-crew-neck-shirt-Mty3tj6h1zo",
-        set: (v)=> v===""?"https://unsplash.com/photos/blue-red-and-white-striped-crew-neck-shirt-Mty3tj6h1zo":v,
-    },
-    price: Number,
-    location: String,
-    country: String,
+const listingSchema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: String,
+
+  image: {
+    filename: { type: String, default: "listingimage" },
+    url: { type: String, default: "https://images.unsplash.com/photo-1625505826533-5c80aca7d157?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fGdvYXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60" }
+  },
+
+  price: Number,
+  location: String,
+  country: String,
 });
 
-const Listing =  mongoose.model("Listing",listingSchema);
-module.exports=Listing;
+const Listing = mongoose.model("Listing", listingSchema);
+module.exports = Listing;
