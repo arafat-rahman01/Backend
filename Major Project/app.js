2252a2cc8ff89ff8144b5a8708de122c9adb2051
arafat-rahman01 <aabrarbin851@gmail.com>
2026-03-14 23:29:03 +0600

    models set(MongoDB)

--- Major Project/app.js
+++ Major Project/app.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const app=express();
 const mongoose=require("mongoose");
+const Listing=require("./models/listing.js")
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
 
@@ -18,6 +19,19 @@ async function main(){
 
 app.get("/",(req,res)=>{
     res.send("Hi, i'm root");
+});
+
+app.get("/testListing",async(req,res)=>{
+    let sampleListing= new Listing({
+        title:" My new villa",
+        description: "By the beach",
+        price:1200,
+        location:"Sugondha, Cox's",
+        country:"Bangladesh",
+    });
+    await sampleListing.save();
+    console.log("Sample was saved");
+    res.send("successful testing");
 });
 
 app.listen(8080,()=>{
---
+++ Major Project/models/listing.js
@@ -0,0 +1,22 @@
+const mongoose=require("mongoose");
+const Schema=mongoose.Schema;
+
+const listingSchema=new Schema({
+    title:{
+        type:String,
+        required:true,
+    },
+    description: String,
+    image:{
+        type: String,
+        default:
+            "https://unsplash.com/photos/blue-red-and-white-striped-crew-neck-shirt-Mty3tj6h1zo",
+        set: (v)=> v===""?"https://unsplash.com/photos/blue-red-and-white-striped-crew-neck-shirt-Mty3tj6h1zo":v,
+    },
+    price: Number,
+    location: String,
+    country: String,
+});
+
+const Listing =  mongoose.model("Listing",listingSchema);
+module.exports=Listing;
