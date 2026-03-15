b3c3884f300c8deab2ebb97010f17f81985f8b0e
arafat-rahman01 <aabrarbin851@gmail.com>
2026-03-15 15:23:05 +0600

    Listings Deatils include

--- Major Project/app.js
+++ Major Project/app.js
@@ -20,6 +20,7 @@ async function main(){
 
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
+app.use(express.urlencoded({extended:true}));
 
 app.get("/",(req,res)=>{
     res.send("Hi, i'm root");
@@ -29,6 +30,13 @@ app.get("/",(req,res)=>{
 app.get("/listings",async(req,res)=>{
     const allListings= await Listing.find({});
     res.render("listings/index.ejs",{allListings})
+});
+
+//Show Route
+app.get("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    const listing=await Listing.findById(id);
+    res.render("listings/show",{listing});
 })
 
 // app.get("/testListing",async(req,res)=>{
