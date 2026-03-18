0d4aa1f98c436e6e8bbf423d3c1993415cb6b97d
arafat-rahman01 <aabrarbin851@gmail.com>
2026-03-18 15:21:06 +0600

    new route & create route

--- Major Project/app.js
+++ Major Project/app.js
@@ -36,6 +36,17 @@ app.get("/listings/new",(req,res)=>{
     res.render("listings/new.ejs");
 });
 
+//Create route
+app.post("/listings",async(req,res)=>{
+   // let {title,description,image,price,country,location}=req.body;
+   //console.log(req.body);
+   //console.log(req.body.listing);
+   const newListing=new Listing(req.body.listing);
+   await newListing.save();
+   res.redirect("/listings");
+
+});
+
 //Show Route
 app.get("/listings/:id",async(req,res)=>{
     let {id}=req.params;
@@ -58,4 +69,4 @@ app.get("/listings/:id",async(req,res)=>{
 
 app.listen(8080,()=>{
     console.log("Serever is listening to port 8080");
-})
+});
