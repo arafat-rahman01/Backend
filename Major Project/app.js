f1c62dc8bb58016cb318564c46694c95a5918ac3
arafat-rahman01 <aabrarbin851@gmail.com>
2026-03-19 15:08:20 +0600

    Updation route working

--- Major Project/app.js
+++ Major Project/app.js
@@ -4,7 +4,7 @@ const mongoose=require("mongoose");
 const Listing=require("./models/listing.js");
 const path=require("path");
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust";
-const methodOverride=
+const methodOverride=require("method-override");
 
 main()
     .then(()=>{
@@ -21,6 +21,8 @@ async function main(){
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+
 
 app.get("/",(req,res)=>{
     res.send("Hi, i'm root");
@@ -62,6 +64,12 @@ app.get("/listings/:id/edit",async(req,res)=>{
     res.render("listings/edit.ejs",{listing});
 });
 
+//update route
+app.put("/listings/:id",async(req,res)=>{
+    let {id}=req.params;
+    await Listing.findByIdAndUpdate(id,{...req.body.listing});
+    res.redirect("/listings");
+});
 
 // app.get("/testListing",async(req,res)=>{
 //     let sampleListing= new Listing({
