c4b97e966c788f78f11154a7c564e8a0e2db05f6
arafat-rahman01 <aabrarbin851@gmail.com>
2026-04-02 01:20:49 +0600

    log in

--- Middleware/app1.js
+++ Middleware/app1.js
@@ -2,18 +2,42 @@ const express = require("express");
 const app = express();
 
 // Middleware
-app.use((req, res, next) => {
-    console.log("Middleware executed!");
-    next();
-});
+// app.use((req, res, next) => {
+//     console.log("Middleware executed!");
+//     next();
+// });
 
 // middleware/auth.js
-module.exports.isLoggedIn = (req, res, next) => {
+// module.exports.isLoggedIn = (req, res, next) => {
+//     if (!req.user) {
+//         return res.send("You must be logged in!");
+//     }
+//     next();
+// };
+
+//Login Route
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+
+    if (username === "arafat" && password === "1234") {
+        req.user = { username };
+        res.send("Login successful");
+    } else {
+        res.send("Invalid credentials");
+    }
+});
+
+const isLoggedIn = (req, res, next) => {
     if (!req.user) {
-        return res.send("You must be logged in!");
+        return res.send("Please login first");
     }
     next();
 };
+
+app.get("/create-listing", isLoggedIn, (req, res) => {
+    res.send("You can create listing");
+});
+
 
 // Route
 app.get("/", (req, res) => {
