303c13a220e5f06ec258bd86967eb85428b464e1
arafat-rahman01 <aabrarbin851@gmail.com>
2026-04-03 01:02:10 +0600

    default error

---
+++ Middleware/app.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const app = express();
+const ExpressError=require("./expressError");
+
+// Middleware
+// app.use((req, res, next) => {
+//     console.log("Middleware executed!");
+//     next();
+// });
+
+// middleware/auth.js
+// module.exports.isLoggedIn = (req, res, next) => {
+//     if (!req.user) {
+//         return res.send("You must be logged in!");
+//     }
+//     next();
+// };
+
+const checkToken=(req,res,next)=>{
+    let {token} = req.query;
+    if(token==="givenaccess"){
+        next();
+    }
+    throw new ExpressError(401,"ACCESS DENIED!");
+};
+
+app.get("/api",checkToken,(req,res)=>{
+    res.send("data");
+});
+
+//Login Route
+// app.post("/login", (req, res) => {
+//     const { username, password } = req.body;
+
+//     if (username === "arafat" && password === "1234") {
+//         req.user = { username };
+//         res.send("Login successful");
+//     } else {
+//         res.send("Invalid credentials");
+//     }
+// });
+
+// const isLoggedIn = (req, res, next) => {
+//     if (!req.user) {
+//         return res.send("Please login first");
+//     }
+//     next();
+// };
+
+// app.get("/create-listing", isLoggedIn, (req, res) => {
+//     res.send("You can create listing");
+// });
+
+
+// Route
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.listen(3000);
