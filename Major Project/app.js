c6b20ff06246c25092e5551d059a8ee71ff8d74b
arafat-rahman01 <aabrarbin851@gmail.com>
2026-04-08 00:21:27 +0600

    error.details.map((el)=>el.message.join(",")

--- Major Project/app.js
+++ Major Project/app.js
@@ -39,9 +39,9 @@ app.get("/",(req,res)=>{
 
 const validateListings=(req,res,next)=>{
     let {error}= listingSchema.validate(req.body);
-    console.log(result);
     if(error){
-        throw new ExpressError(400,result.error);
+        let errMsg=error.details.map((el)=>el.message.join(","));
+        throw new ExpressError(400,errMsg);
     }else{
         next();
     }
