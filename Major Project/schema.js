21ded4a736e9bc73fcd1beb2ca352ba2411318f9
arafat-rahman01 <aabrarbin851@gmail.com>
2026-04-11 13:44:45 +0600

    joi bug fixed

--- Major Project/schema.js
+++ Major Project/schema.js
@@ -1,12 +1,15 @@
-const Joi=require("joi");
+const Joi = require("joi");
 
-module.exports.listingSchema=Joi.object({
-    listings: Joi.object({
+module.exports.listingSchema = Joi.object({
+    listing: Joi.object({
         title: Joi.string().required(),
         description: Joi.string().required(),
         location: Joi.string().required(),
         country: Joi.string().required(),
         price: Joi.number().required().min(0),
-        image: Joi.string().allow("",null)
-    }).required()
-})
+        image: Joi.object({
+            url: Joi.string().allow("", null),
+            filename: Joi.string().allow("", null)
+            }).allow(null)
+        }).required()
+});
