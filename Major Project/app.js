7557a0dedc30f008ab7efdd6b3b3ece7175afaa5
arafat-rahman01 <aabrarbin851@gmail.com>
2026-04-11 13:39:04 +0600

    Price Updation problem solved

--- Major Project/app.js
+++ Major Project/app.js
@@ -37,15 +37,16 @@ app.get("/",(req,res)=>{
     res.send("Hi, i'm root");
 });
 
-const validateListings=(req,res,next)=>{
-    let {error}= listingSchema.validate(req.body);
-    if(error){
-        let errMsg=error.details.map((el)=>el.message.join(","));
-        throw new ExpressError(400,errMsg);
-    }else{
+const validateListings = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(", ");
+        throw new ExpressError(400, errMsg);
+    } else {
         next();
     }
-}
+};
 
 //index route
 app.get("/listings",wrapAsync(async(req,res)=>{
