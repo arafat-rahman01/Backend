60a4a6bfb3fc5024a8b24d264bf54173641f9429
arafat-rahman01 <aabrarbin851@gmail.com>
2026-04-12 12:27:45 +0600

    Review

--- Major Project/app.js
+++ Major Project/app.js
@@ -9,6 +9,7 @@ const ejsMate=require("ejs-mate");
 const wrapAsync=require("./utils/wrapAsync.js");
 const ExpressError=require("./utils/ExpressError.js");
 const { listingSchema }=require("./schema.js");
+const Review=require("./models/review.js");
 
 main()
     .then(()=>{
@@ -112,6 +113,19 @@ app.delete("/listings/:id",wrapAsync(async(req,res)=>{
     console.log(deletedlisting);
     res.redirect("/listings");
 }));
+
+//Reviews
+app.post("/listings/:id/reviews",async(req,res)=>{
+    let listing = await Listing.findById(req.params.id);
+    let newReview = new Review(req.body.review);
+
+    listing.reviews.push(newReview);
+
+    await newReview.save();
+    await listing.save();
+
+    res.redirect(`/listings/${listing._id}`);
+});
 
 // app.get("/testListing",async(req,res)=>{
 //     let sampleListing= new Listing({
