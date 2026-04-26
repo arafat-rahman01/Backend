9e71df3055910204a9c37e38d4d363a4733445c8
arafat-rahman01 <aabrarbin851@gmail.com>
2026-04-26 22:59:52 +0600

    failureRedirect &  key=success (post method  Login passport.authenticate)

--- Major Project/routes/user.js
+++ Major Project/routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync.js");
+const passport= require("passport");
 
 router.get("/signup",(req,res)=>{
     res.render("users/signup.ejs");
@@ -24,6 +25,17 @@ router.post("/signup",wrapAsync(async(req,res)=>{
 
 router.get("/login",(req,res)=>{
     res.render("users/login");
-})
+});
+
+router.post(
+    "/login", 
+    passport.authenticate("local",
+        { failureRedirect: '/login',
+            failureFlash: true
+        }) 
+        ,async(req,res)=>{
+            res.flash("success","Welcome to Wonderlust! You are logged in!");
+        }
+)
 
 module.exports = router;
