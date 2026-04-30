d456d00a62ba90f8301eeb2bbfcefa88045d0243
arafat-rahman01 <aabrarbin851@gmail.com>
2026-05-01 00:03:17 +0600

    all module include in middleware

--- Major Project/middleware.js
+++ Major Project/middleware.js
@@ -1,4 +1,6 @@
 const Listing = require("./models/listing");
+const { listingSchema,reviewSchema}=require("./schema.js");
+const ExpressError=require("./utils/ExpressError.js");
 
 module.exports.isLoggedIn = (req,res,next)=>{
     if(!req.isAuthenticated()){
@@ -24,4 +26,26 @@ module.exports.isOwner =async (req,res,next)=>{
         return res.redirect(`/listings/${id}`);
     }
     next();
+};
+
+module.exports.validateListings = (req, res, next) => {
+    let { error } = listingSchema.validate(req.body);
+
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(", ");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
+};
+
+module.exports.validateReview = (req, res, next) => {
+    let { error } = reviewSchema.validate(req.body);
+
+    if (error) {
+        let errMsg = error.details.map((el) => el.message).join(", ");
+        throw new ExpressError(400, errMsg);
+    } else {
+        next();
+    }
 };
--- Major Project/routes/listing.js
+++ Major Project/routes/listing.js
@@ -2,21 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Listing=require("../models/listing.js");
 const wrapAsync=require("../utils/wrapAsync.js");
-const { listingSchema}=require("../schema.js");
-const ExpressError=require("../utils/ExpressError.js");
-const {isLoggedIn} = require("../middleware.js");
-const { isOwner } = require("../middleware.js");
-
-const validateListings = (req, res, next) => {
-    let { error } = listingSchema.validate(req.body);
-
-    if (error) {
-        let errMsg = error.details.map((el) => el.message).join(", ");
-        throw new ExpressError(400, errMsg);
-    } else {
-        next();
-    }
-};
+const { isLoggedIn,isOwner,validateListings } = require("../middleware.js");
 
 //index route
 router.get(
