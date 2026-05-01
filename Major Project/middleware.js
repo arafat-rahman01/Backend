0bbb69aa9d79c5b327b25f9ba0c1100b42177951
arafat-rahman01 <aabrarbin851@gmail.com>
2026-05-01 23:20:01 +0600

    bug fixed

--- Major Project/middleware.js
+++ Major Project/middleware.js
@@ -62,7 +62,7 @@ module.exports.validateReview = (req, res, next) => {
     }
 };
 
-module.exports.isReviewAuthor = (req, res, next) => {
+module.exports.isReviewAuthor = async(req, res, next) => {
     let { id,reviewId } = req.params;
     let review = await Review.findById(reviewId);
 
